9a0fb7125977012ef59053e10d2262221115614c
AbdulsProjects <106890796+AbdulsProjects@users.noreply.github.com>
2023-05-01 00:18:10 +0100

    Altered the method [returnFlipOrder] to work when running consecutive instances. Added a new conditional style to the tile div boxes to reflect those that need to be flipped.

--- KephriWebApp/Pages/Index.razor.cs
+++ KephriWebApp/Pages/Index.razor.cs
@@ -8,22 +8,32 @@ namespace KephriWebApp.Pages
         private bool[] flippedTiles = { false, false, false, false, false, false, false, false, false };
         private bool[] solvedFlipOrder = { false, false, false, false, false, false, false, false, false };
 
+        //Allowing the user to flip tiles
         private void ChangeStartState(int index)
         {
             flippedTiles[index] ^= true;
         }
 
+        //Formatting tiles flipped by the user
         private string StateStyle(bool flipped)
         {
             if (flipped) return "flipped-tile";
             return "";
         }
 
+        //Formatting tiles that need to be flipped to solve the puzzle
+        private string BorderStyle(bool requiresFlip)
+        {
+            if (requiresFlip) return "requires-flip";
+            return "";
+        }
+
         private void returnFlipOrder()
         {
             bool[,] formattedFlippedTiles = { { flippedTiles[0], flippedTiles[1], flippedTiles[2] }, { flippedTiles[3], flippedTiles[4], flippedTiles[5] }, { flippedTiles[6], flippedTiles[7], flippedTiles[8] } };
-         
-            
+            //Resetting the solved array
+            for (int i = 0; i < solvedFlipOrder.Length; ++i) { solvedFlipOrder[i] = false; }
+
             int[] returnedFlipOder = LibraryProgram.SimulateGame(formattedFlippedTiles).Item2;
 
             //Converting the solvedFlipOrder into a single dimension array for use in the UI 
